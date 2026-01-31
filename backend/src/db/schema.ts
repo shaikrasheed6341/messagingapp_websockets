6943bc40b925c3ef6180b586792e46954d7ae517
Shaik Rasheed <shaikrasheed6341@gmail.com>
2026-01-31 19:11:09 +0530

    schema is create for messageing

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,10 +1,49 @@
+import { primaryKey } from "drizzle-orm/pg-core";
+import { text } from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/pg-core";
 import { varchar } from "drizzle-orm/pg-core";
 import { uuid } from "drizzle-orm/pg-core";
 import { pgTable, PgTable } from "drizzle-orm/pg-core";
  
-export const User = pgTable("users",{
+export const users = pgTable("users",{
     id:uuid("id").primaryKey().defaultRandom(),
     name:varchar("name").notNull(),
-    email:varchar("")
+    email:varchar("email").unique().notNull(),
+    password:varchar("password").notNull(),
+    createdAt:timestamp("createAt").defaultNow()
 
 })
+ export const rooms = pgTable("rooms",{
+    id:uuid("id").primaryKey().defaultRandom(),
+    roomcode:varchar("roomcode",{length:4}).notNull().unique(),
+    roompassword:varchar("roompassword").notNull(),
+    createdAt:timestamp("createAt").defaultNow()
+
+
+ })
+
+ export const roomparticipants = pgTable("roomparticipants",{
+      roomid : uuid("roomid")
+      .notNull()
+      .references(()=> rooms.id),
+      userid:uuid("userid")
+      .notNull()
+      .references(()=> users.id)
+ },
+    (table)=>({
+        pk:primaryKey({columns:[table.roomid,table.roomid]})
+    })
+ )
+
+ export const message = pgTable("message",{
+    roomid:uuid("roomid")
+    .notNull()
+    .references(()=>rooms.id),
+
+    sederid:uuid("senderid")
+    .notNull()
+    .references(()=> users.id),
+    constext:text().notNull(),
+    createdAt:timestamp("createdAt").defaultNow()
+
+ })
